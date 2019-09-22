d888ed7c1c78d59ea11d56c72235cb6de7679933
Luka Negoita <lukanegoita@gmail.com>
2019-09-22 12:08:22 -0600

    Reverted from the past

--- scripts/00_functions.R
+++ scripts/00_functions.R
@@ -3,11 +3,4 @@
 #####################################
 
 library(tidyverse)
-<<<<<<< HEAD
 library(lubridate)
-
-## Need to add this in too (fixed mistake)
-
-## another change
-=======
->>>>>>> parent of e8c2444... add code to upload the lubridate library
