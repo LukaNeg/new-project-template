a38cb6960e21fbf8f4a45aa68a41bbeeeeb6906e
Luka Negoita <lukanegoita@gmail.com>
2019-09-22 12:04:43 -0600

    reverting  test again

--- scripts/00_functions.R
+++ scripts/00_functions.R
@@ -3,8 +3,11 @@
 #####################################
 
 library(tidyverse)
+<<<<<<< HEAD
 library(lubridate)
 
 ## Need to add this in too (fixed mistake)
 
 ## another change
+=======
+>>>>>>> parent of e8c2444... add code to upload the lubridate library
