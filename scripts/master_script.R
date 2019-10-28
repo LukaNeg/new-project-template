d1f56aa89c6a855357dda4258cdddc7e7d60d134
Luka Negoita <54965301+LukaNeg@users.noreply.github.com>
2019-10-28 14:16:32 -0600

    inital commit for master script

---
+++ scripts/master_script.R
@@ -0,0 +1,15 @@
+###########################################
+##### MASTER SCRIPT TO RULE THEM ALL ######
+###########################################
+
+source("scripts/00_functions.R")
+source("scripts/01_dataclean.R")
+source("scripts/02_analysis.R")
+
+### Load data:
+
+
+### Run cleaning function scripts:
+
+
+### Run analysis and plotting scripts:
