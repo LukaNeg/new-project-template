ad9a4077236a1b25f29139ef541a0eff49fbf398
Luka Negoita <54965301+LukaNeg@users.noreply.github.com>
2019-10-28 14:40:53 -0600

    add functions

--- scripts/00_functions.R
+++ scripts/00_functions.R
@@ -6,4 +6,17 @@
 library(tidyverse)
 library(lubridate)
 
-### Load custom functions:
+##############################
+### Load custom functions: ###
+
+### Transparent colors function:
+t_col <- function(color, percent = 50) {
+  rgb.val <- col2rgb(color)
+  t.col <- rgb(rgb.val[1], rgb.val[2], rgb.val[3], max = 255, alpha = (100-percent)*255/100)
+  invisible(t.col)
+}
+
+### function to standardize continuous variables for effect size comparison to logical variables
+### and to ensure standardization within each species for global comparison
+### https://www.listendata.com/2017/04/how-to-standardize-variable-in-regression.html
+standard <- function(x) (x - mean(x, na.rm=T)) / (2*sd(x, na.rm=T))
